27423e6fc92517a17049e603bf95aac93d78da73
Yusuke Michiue <c011449032@edu.teu.ac.jp>
2017-12-08 18:16:52 +0900

    feef

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
-
+import serial
+from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def hello():
     #return name
     #return render_template('index.html', title='flask test', name=name) #変更
-    return render_template('index.html')
+    return render_template('hello.html')
 
-## おまじない
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/sendtext',methods=['POST'])
+def sendMeganeMode():
+
+    meganeMode = request.form['message']
+
+app.run(debug=True)
