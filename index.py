19444945b4eb7c10f97d0de4985607223d08e56c
Yusuke Michiue <c011449032@edu.teu.ac.jp>
2017-12-11 17:03:14 +0900

    いらんファイル削除した

--- index.py
+++ index.py
@@ -5,8 +5,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    #return name
-    #return render_template('index.html', title='flask test', name=name) #変更
     return render_template('hello.html')
 
 @app.route('/sendtext',methods=['POST'])
